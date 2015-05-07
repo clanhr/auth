5a621ed54e21c268bc3a99e9f8b571a7ad4ebbb5
Marcos <marcoslamuria@gmail.com>
2015-05-07 15:50:05 +0100

    fix license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject auth "0.1.0"
   :description "FIXME: write description"
   :url "https://github.com/clanhr/auth"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "file://LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [environ "1.0.0"]
                  [clj-jwt "0.0.13"]
