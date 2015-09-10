e532e0eb6c00b272a6e391cc59b3427904c8e468
Marcos <marcoslamuria@gmail.com>
2015-09-10 14:50:01 +0100

    extract user-id from token

--- test/clanhr/auth/auth_middleware_test.clj
+++ test/clanhr/auth/auth_middleware_test.clj
@@ -25,14 +25,3 @@
         (let [response ((auth-middleware/run handler) (request :get "/"))]
           (is (= 401
                  (:status response))))))))
-
-(deftest extract-data-test
-  (let [data {:user "bob_the_builder"
-              :password "spoon"
-              :email "bubu@mail.com"
-              :account "account"
-              :user-id "user_id"}
-        token (auth/token-for data)
-        result-valid (auth-middleware/valid? token)
-        result (auth-middleware/add-principal {} result-valid)]
-    (is (get-in result [:principal :user-id]))))
