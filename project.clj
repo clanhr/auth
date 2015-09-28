3daa52b7a883fd1a193e059b3bb65e542a1a4f7f
Marcos <marcoslamuria@gmail.com>
2015-09-28 21:28:42 +0100

    upgrade version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/auth "0.4.4"
+(defproject clanhr/auth "0.4.5"
   :description "ClanHR's Auth Library"
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
