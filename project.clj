f9e12fc309abb5d1790a0c089943f19215f4ee5a
Pedro Santos <donbonifacio@gmail.com>
2015-12-17 10:32:56 +0000

    Bump deps

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "The MIT License"
             :url "file://LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [environ "1.0.0"]
-                 [ring/ring-core "1.3.2"]
-                 [ring/ring-mock "0.2.0"]
+                 [environ "1.0.1"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-mock "0.3.0"]
                  [clanhr/result "0.10.3"]
-                 [clanhr/reply "0.7.0"]
-                 [clanhr/clanhr-api "1.1.0"]
-                 [clj-jwt "0.0.13"]
-                 [clj-time "0.9.0"]]
+                 [clanhr/reply "0.9.0"]
+                 [clanhr/clanhr-api "1.2.0"]
+                 [clj-jwt "0.1.1"]
+                 [clj-time "0.11.0"]]
   :plugins [[lein-environ "1.0.0"]]
   :profiles {:test {:env {:secret "test_secret"}}
              :dev {:env {:secret "dev_secret"}}})
