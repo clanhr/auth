a32db19a0e036159f1e933da390208bce3c0cd65
Marcos <marcoslamuria@gmail.com>
2016-01-11 12:24:15 +0000

    add empty roles to valid authorization roles

--- src/clanhr/auth/authorization_rules.clj
+++ src/clanhr/auth/authorization_rules.clj
@@ -2,7 +2,7 @@
   (:require [result.core :as result]))
 
 (def rules
-  {:notifications-access ["admin" "hrmanager" "manager" "user"]
+  {:notifications-access ["admin" "hrmanager" "manager" "user" ""]
    :reports-access ["admin" "hrmanager" "manager"]
    :can-manage-absences ["admin" "hrmanager"]
    :delete-user ["admin" "hrmanager"]})
@@ -10,6 +10,9 @@
 (defn run
   "Check if role can perform action"
   [action roles]
-  (if (some (set roles) (action rules))
-    (result/success {:roles roles})
-    (result/forbidden)))
+  (let [roles (if (or (nil? roles) (empty? roles))
+                [""]
+                roles)]
+    (if (some (set roles) (action rules))
+      (result/success {:roles roles})
+      (result/forbidden))))
