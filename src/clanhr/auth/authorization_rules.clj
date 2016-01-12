d590b81663e7fc4ee056b8f4b9dac4437d1fa2de
Marcos <marcoslamuria@gmail.com>
2016-01-12 15:13:19 +0000

    add access to staff members to all features

--- src/clanhr/auth/authorization_rules.clj
+++ src/clanhr/auth/authorization_rules.clj
@@ -2,10 +2,10 @@
   (:require [result.core :as result]))
 
 (def rules
-  {:notifications-access ["admin" "hrmanager" "manager" "user" ""]
-   :reports-access ["admin" "hrmanager" "manager"]
-   :can-manage-absences ["admin" "hrmanager"]
-   :delete-user ["admin" "hrmanager"]})
+  {:notifications-access ["admin" "hrmanager" "manager" "user" "" "staff"]
+   :reports-access ["admin" "hrmanager" "manager" "staff"]
+   :can-manage-absences ["admin" "hrmanager" "staff"]
+   :delete-user ["admin" "hrmanager" "staff"]})
 
 (defn run
   "Check if role can perform action"
