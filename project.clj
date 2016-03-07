cba8c5424cea0e3b9bb3cd776a5336848b1dbc1c
Pedro Santos <donbonifacio@gmail.com>
2016-03-07 17:16:55 +0000

    If no directory api on env, don't try to call the service

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/auth "1.11.0"
+(defproject clanhr/auth "1.11.1"
   :description "ClanHR's Auth Library"
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
