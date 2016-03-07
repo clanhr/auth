ffbf5a1e82c1b755accfae9b9c39f8eac8496f84
Pedro Santos <donbonifacio@gmail.com>
2016-03-07 14:51:44 +0000

    Update project to use shared dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
             :url "file://LICENSE"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [environ "1.0.1"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-mock "0.3.0"]
-                 [clanhr/result "0.10.3"]
-                 [clanhr/reply "0.9.0"]
-                 [clanhr/clanhr-api "1.2.0"]
-                 [clj-jwt "0.1.1"]
-                 [clj-time "0.11.0"]]
-  :plugins [[lein-environ "1.0.0"]]
+  :dependencies.edn "https://raw.githubusercontent.com/clanhr/dependencies/master/dependencies.edn"
+  :dependencies []
+
+  :dependency-sets [:clojure
+                    :security
+                    :common
+                    :ring
+                    :clanhr]
+  :plugins [[lein-environ "1.0.0"]
+            [clanhr/shared-deps "0.2.6"]]
   :profiles {:test {:env {:secret "test_secret"}}
              :dev {:env {:secret "dev_secret"}}})
