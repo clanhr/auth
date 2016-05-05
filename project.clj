2591d7cf8b53903a76f35b20114d04c8a544b2e0
Pedro Santos <donbonifacio@gmail.com>
2016-05-05 12:03:35 +0100

    Also consider api_key as token source
    Motivation:
    
    The swagger ui uses *api_key* as a query string param for auth. Because
    I don't know how to change that, we're also supporting this param.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/auth "1.11.5"
+(defproject clanhr/auth "1.12.0"
   :description "ClanHR's Auth Library"
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
