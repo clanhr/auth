de6195c51edfdbd7f4c977ea649f946a194aacb2
Pedro Santos <donbonifacio@gmail.com>
2016-07-27 11:49:23 +0100

    Bump version to 1.13.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/auth "1.12.0"
+(defproject clanhr/auth "1.13.0"
   :description "ClanHR's Auth Library"
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
