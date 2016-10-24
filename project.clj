ce484d16d809167740fbcac67143a7b806187045
claudiaalves <claudia.alves@rupeal.com>
2016-10-24 14:42:37 +0100

    Add a rule for mark an account as paid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/auth "1.15.0"
+(defproject clanhr/auth "1.16.0"
   :description "ClanHR's Auth Library"
   :url "https://github.com/clanhr/auth"
   :license {:name "The MIT License"
--- src/clanhr/auth/authorization_rules.clj
+++ src/clanhr/auth/authorization_rules.clj
@@ -5,7 +5,8 @@
   "Template role profiles for easy access"
   {:full-access ["admin" "hrmanager" "manager" "user" "" "staff"]
    :board-member-manager ["admin" "hrmanager" "manager" "staff"]
-   :board-member ["admin" "hrmanager" "staff"]})
+   :board-member ["admin" "hrmanager" "staff"]
+   :developer-member ["developer"]})
 
 (def ^:const rules
   "Maps specific actions or zones to allowed roles"
@@ -18,6 +19,7 @@
    :can-manage-settings (:board-member profile)
    :can-manage-alerts (:board-member profile)
    :can-manage-holidays (:board-member profile)
+   :can-mark-account-as-paid (:developer-member profile)
    :change-absence-state (conj (:board-member profile) "approver")
    :settings-access (:board-member profile)
    :can-see-full-user-info (:board-member profile)
