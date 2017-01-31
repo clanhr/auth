4d926809f971d34e3c045b3b9d86df9ec66999f2
Marcos Lamúria <marcoslamuria@gmail.com>
2017-01-31 15:36:35 +0000

    Add self-approver utility and board-member to expenses rules
    - Add self-approver utility. This utility receives 2 users and checks if
      they are the same, if they are the same checks if the user is it's own
      approver
    
      - Add a board-member to expenses rules

--- src/clanhr/auth/roles_for.clj
+++ src/clanhr/auth/roles_for.clj
@@ -11,9 +11,23 @@
             (some #{user-id} (get-in other-user [:company-data :manager-ids])))
       "approver")))
 
+(defn self-approver
+  "If the user is the same as other-user and the user is approver of himself,
+   returns the self-approver role"
+  [user other-user]
+  (if-let [user-id (:_id user)]
+    (when (and (= user-id (:_id other-user))
+             (some #{user-id} (get-in user [:company-data :manager-ids])))
+      "self-approver")))
+
+(defn get-roles
+  [user other-user]
+  (conj [(approver user other-user)]
+         (self-approver user other-user)))
+
 (defn run
   "Returns specific roles that represent the relation between user and other-user"
   [{:keys [user other-user]}]
-  (->> [(approver user other-user)]
+  (->> (get-roles user other-user)
        (filter identity)
        (result/success)))
