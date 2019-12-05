3c2b2f6f77a1ccd00bbb3b84c3446600d58b429d
Paulo Silva <paulosilva86@gmail.com>
2019-12-05 16:15:21 +0000

    Allow self-approver to approve pending expenses
    Why:
    
    * If a self-approver has a pending expense, he/she should be able to approve it.
    
    This change addresses the need by:
    
    * Allowing a self-approver on the rule :can-auto-approve-expenses.

--- src/clanhr/auth/authorization_rules.clj
+++ src/clanhr/auth/authorization_rules.clj
@@ -18,6 +18,7 @@
    :administrator-member ["admin" "hrmanager"]})
 
 (def ^:const approver "approver")
+(def ^:const self-approver "self-approver")
 (def ^:const expenses-manager "expensesManager")
 (def ^:const absences-manager "absencesManager")
 
@@ -35,7 +36,7 @@
    :can-mark-account-as-paid (:developer-member profile)
    :change-absence-state (conj (:board-member profile) approver absences-manager)
    :change-expense-state (conj (:board-member profile) approver expenses-manager)
-   :can-auto-approve-expenses (conj (:board-member profile) approver expenses-manager)
+   :can-auto-approve-expenses (conj (:board-member profile) approver self-approver expenses-manager)
    :settings-access (conj (:board-member profile) absences-manager expenses-manager)
    :can-see-full-user-info (:board-member profile)
    :delete-user (:board-member profile)
